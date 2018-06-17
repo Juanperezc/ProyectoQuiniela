f6b4ca03cade86af13906ebab23c71b5fe5ae9b9
Juan Perez <juanl1996@hotmail.com>
2018-06-17 18:11:00 -0400

    Agregando Hibernate

--- src/main/java/com/lab2/ProyectoQuinielaApplication.java
+++ src/main/java/com/lab2/ProyectoQuinielaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoQuinielaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoQuinielaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProyectoQuinielaApplication.class, args);
+  }
 }
--- src/main/java/com/lab2/controlador/Controlador.java
+++ src/main/java/com/lab2/controlador/Controlador.java
@@ -1,84 +1,105 @@
 package com.lab2.controlador;
 
+import java.util.List;
+import com.lab2.modelo.User;
+import com.lab2.repositorio.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
 //
 @Controller
 public class Controlador {
 
+
+
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
-	
+
 	@RequestMapping("/index")
 	public String showIndex() {
 		return "index";
 	}
-	
+
 	@RequestMapping("/login")
 	public String showLogin() {
 		return "login";
 	}
+
 	@RequestMapping("/quinielaFootballPublica")
 	public String showquinielafootballpublica() {
 		return "quinielafootballpublica";
 	}
+
 	@RequestMapping("/quinielaBaseballPublica")
 	public String showquinielabaseballpublica() {
 		return "quinielabaseballpublica";
 	}
+
 	@RequestMapping("/quinielaMundialPublica")
 	public String showquinielamundialpublica() {
 		return "quinielamundialpublica";
 	}
+
 	@RequestMapping("/quinielaMundialPrivada")
 	public String showquinielamundialprivada() {
 		return "quinielamundialprivada";
 	}
+
 	@RequestMapping("/register")
 	public String showRegister() {
 		return "register";
 	}
+
 	@RequestMapping("/profile")
 	public String showProfile() {
 		return "profile";
 	}
+
 	@RequestMapping("/request")
 	public String showRequest() {
 		return "request";
 	}
+
 	@RequestMapping("/report")
 	public String showReport() {
 		return "report";
 	}
+
 	@RequestMapping("/manage_accounts")
 	public String showMAccount() {
 		return "manage_accounts";
 	}
-	
+
 	@RequestMapping("/crearQuiniela")
 	public String showCrearQuiniela() {
 		return "crearQuiniela";
 	}
-	
+
 	@RequestMapping("/misQuinielas")
 	public String showmisQuinielas() {
 		return "misquinielas";
 	}
+
 	@RequestMapping("/verQuinielas")
 	public String showverQuinielas() {
 		return "verquinielas";
 	}
+
 	@RequestMapping("/modificarQuinielas")
 	public String showmodificarQuinielas() {
 		return "modificarQuinielas";
 	}
+
 	@RequestMapping("/pag2")
 	public String showPag2() {
 		return "500";
 	}
-	
-	
-	
+
 }
